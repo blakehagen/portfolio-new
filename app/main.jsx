291c1161a888e594aaa7ce7f4f1c8dc3740c1d5b
Blake Hagen <blakehagen@gmail.com>
2018-01-07 15:19:56 -0700

    building out

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import App from '../app/components/App';
 
 import styles from './main.scss';
 
 ReactDOM.render(
-  <div className={styles.container}>
-    Hello World! NEW PORTFOLIO!
-    {console.log('hello there!')}
+  <div className={styles.appBody}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </div>, document.getElementById('app'),
 );
