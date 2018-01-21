d9526e9c903a2aeeeb364c7a43929cb6544e8e84
Blake Hagen <blakehagen@gmail.com>
2018-01-21 13:06:59 -0700

    header

--- app/components/TopBar/TopBar.jsx
+++ app/components/TopBar/TopBar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-// import HeaderImage from './HeaderImage';
 import styles from './topbar.scss';
 
 const TopBar = () => {
+  const github = <a href="https://github.com/blakehagen" rel="noopener noreferrer" target="_blank"><i className="fa fa-github" aria-hidden="true" /></a>;
+  const linkedIn = <a href="https://linkedin.com/in/blakehagen" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin" aria-hidden="true" /></a>;
+  const twitter = <a href="https://twitter.com/blakehagen" rel="noopener noreferrer" target="_blank"><i className="fa fa-twitter" aria-hidden="true" /></a>;
+  const contact = <i className="fa fa-envelope-o" aria-hidden="true" />;
+
   return (
     <div className={styles.topBarWrapper}>
       <div className={styles.topBarActual}>
-        <div className={styles.box}>
-          something here
+        <div className={styles.left}>
+          {/*Blake Hagen*/}
         </div>
 
-        <div className={styles.box}>
-          something here
+        <div className={styles.iconContainer}>
+          {github}
+          {linkedIn}
+          {twitter}
+          {contact}
         </div>
 
       </div>
