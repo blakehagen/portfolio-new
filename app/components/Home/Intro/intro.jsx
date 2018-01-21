f83d82aff5f23099ec309526c79ba4455b888c62
Blake Hagen <blakehagen@gmail.com>
2018-01-20 22:39:37 -0700

    better Intro

--- app/components/Home/Intro/intro.jsx
+++ app/components/Home/Intro/intro.jsx
@@ -3,6 +3,12 @@ import styles from './intro.scss';
 
 const Intro = () => {
   const mePic = require('./images/profile_bw-compressor.jpg');
+  const skills = ['JavaScript', 'React', 'Angular', 'Node', 'SQL', 'PostgreSQL'];
+  const skillTags = _.map(skills, (skill, index) => (
+    <div key={index} className={styles.skillTag}>
+      {skill}
+    </div>
+  ));
 
   return (
     <div className={styles.wrapper}>
@@ -19,16 +25,10 @@ const Intro = () => {
         </div>
         <div className={styles.description}>
           Hi, I'm Blake. I'm a full-stack JavaScript developer based out of the Provo, UT area and I like to build stuff.
-          I'm a husband, father, coder, triathlete, sports nut, and lover of faith, food, and fun.
-          <br /> <br />
-          I have experience with
-          <span className={styles.skillTag}> React</span>,
-          <span className={styles.skillTag}> Webpack</span>,
-          <span className={styles.skillTag}> MobX</span>,
-          <span className={styles.skillTag}> Angular 1.x</span>,
-          <span className={styles.skillTag}> PostgreSQL</span>,
-          <span className={styles.skillTag}> Node</span>,
-          and more. Check out my stuff and holler at me if you'd like to work together on something.
+          I'm a husband, father, coder, triathlete, sports nut, and lover of faith, food, and fun. Holler at me if you'd like to work on something together.
+          <div className={styles.tagContainer}>
+            {skillTags}
+          </div>
         </div>
       </div>
     </div>
