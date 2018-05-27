1107f7268ff425afd07199f5a9363873407a15e9
Blake Hagen <blakehagen@gmail.com>
2018-05-27 13:01:10 -0600

    responsive intro

--- app/components/Home/Intro/Intro.jsx
+++ app/components/Home/Intro/Intro.jsx
@@ -10,26 +10,38 @@ const Intro = () => {
     </div>
   ));
 
+  const adjustedMePic = (
+    <div className={styles.smallImageWrapper}>
+      <img src={mePic} alt="Blake Hagen" />
+    </div>
+  );
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.imageWrapper}>
         <img src={mePic} alt="Blake Hagen" />
       </div>
 
-      <div className={styles.textWrapper}>
-        <div className={styles.firstName}>
-          <p>Blake <span className={styles.lastName}>Hagen</span></p>
+      <div className={styles.introContentWrapper}>
+        <div className={styles.headerWrapper}>
+          {adjustedMePic}
+          <div className={styles.nameTitleWrapper}>
+            <div className={styles.firstName}>
+              <p>Blake <span className={styles.lastName}>Hagen</span></p>
+            </div>
+            <div className={styles.title}>
+              <p>Full-stack Developer</p>
+            </div>
+          </div>
         </div>
-        <div className={styles.tag}>
-          <p>Full-stack Developer</p>
-        </div>
+
         <div className={styles.description}>
           <p>
             Hi, I'm Blake. I'm a full-stack JavaScript developer based in the Provo, UT area.
             I'm a husband, father, coder, triathlete, food lover, and sports nut. I currently work as a developer at&nbsp;
             <a href="https://www.grow.com/" target="_blank" rel="noopener noreferrer">Grow.com</a>. Holler at me if you'd like to work on something together.
           </p>
-        <div className={styles.tagContainer}>
+          <div className={styles.tagContainer}>
             {skillTags}
           </div>
         </div>
